6231e4b921986ce0a56901c37758c511efa8a148
fuck <fuck@qq.com>
2023-02-15 23:06:10 +0800

    My First Commit to GitHub ! I am so excited !

---
+++ FirstCommit/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName:HelloWorld
+ * Package:PACKAGE_NAME
+ * Date:15/02/2023 11:02 PM
+ * Description:
+ * Author:1163070713@qq.com
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
